b9337ec18f2d0e891d03827a63a52060d8c916aa
Ignacio Martinez <ignacio@protonmail.com>
2018-10-22 19:29:36 +0000

    fixes bug: `rb <- wbart(x.train = x, y.train = y, x.test = xtest, nskip = 500, ndpost = 2000)` did not work before

--- R/wbart.R
+++ R/wbart.R
@@ -45,7 +45,10 @@ if(!transposed) {
     numcut = temp$numcut
     xinfo = temp$xinfo
     if(length(x.test)>0)
-            x.test = t(bartModelMatrix(x.test[ , temp$rm.const]))
+      if(length(x.test)>0) {
+        x.test = bartModelMatrix(x.test)
+        x.test = t(x.test[ , temp$rm.const])
+      }
     rm.const <- temp$rm.const
     grp <- temp$grp
     rm(temp)
